388013ed690529a6a26b30ace07d3ac2bd2f469c
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2023-08-19 15:56:20 +0800

    feat: Added string query and test cases

--- src/tests/unit/test-handler.test.ts
+++ src/tests/unit/test-handler.test.ts
@@ -1,65 +1,83 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyEventQueryStringParameters } from 'aws-lambda';
 import { lambdaHandler } from '../../app';
 import { expect, describe, it } from '@jest/globals';
 
-describe('Unit test for app handler', function () {
-    it('verifies successful response', async () => {
-        const event: APIGatewayProxyEvent = {
-            httpMethod: 'get',
-            body: '',
-            headers: {},
-            isBase64Encoded: false,
-            multiValueHeaders: {},
-            multiValueQueryStringParameters: {},
-            path: '/hello',
-            pathParameters: {},
-            queryStringParameters: {},
-            requestContext: {
-                accountId: '123456789012',
-                apiId: '1234',
-                authorizer: {},
-                httpMethod: 'get',
-                identity: {
-                    accessKey: '',
-                    accountId: '',
-                    apiKey: '',
-                    apiKeyId: '',
-                    caller: '',
-                    clientCert: {
-                        clientCertPem: '',
-                        issuerDN: '',
-                        serialNumber: '',
-                        subjectDN: '',
-                        validity: { notAfter: '', notBefore: '' },
-                    },
-                    cognitoAuthenticationProvider: '',
-                    cognitoAuthenticationType: '',
-                    cognitoIdentityId: '',
-                    cognitoIdentityPoolId: '',
-                    principalOrgId: '',
-                    sourceIp: '',
-                    user: '',
-                    userAgent: '',
-                    userArn: '',
-                },
-                path: '/hello',
-                protocol: 'HTTP/1.1',
-                requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
-                requestTimeEpoch: 1428582896000,
-                resourceId: '123456',
-                resourcePath: '/hello',
-                stage: 'dev',
-            },
-            resource: '',
-            stageVariables: {},
-        };
-        const result: APIGatewayProxyResult = await lambdaHandler(event);
+const createEvent = (queryStringParameters: APIGatewayProxyEventQueryStringParameters): APIGatewayProxyEvent => ({
+  httpMethod: 'get',
+  body: '',
+  headers: {},
+  isBase64Encoded: false,
+  multiValueHeaders: {},
+  multiValueQueryStringParameters: {},
+  path: '/scrape',
+  pathParameters: {},
+  queryStringParameters,
+  requestContext: {
+    accountId: '123456789012',
+    apiId: '1234',
+    authorizer: {},
+    httpMethod: 'get',
+    identity: {
+      accessKey: '',
+      accountId: '',
+      apiKey: '',
+      apiKeyId: '',
+      caller: '',
+      clientCert: {
+        clientCertPem: '',
+        issuerDN: '',
+        serialNumber: '',
+        subjectDN: '',
+        validity: { notAfter: '', notBefore: '' },
+      },
+      cognitoAuthenticationProvider: '',
+      cognitoAuthenticationType: '',
+      cognitoIdentityId: '',
+      cognitoIdentityPoolId: '',
+      principalOrgId: '',
+      sourceIp: '',
+      user: '',
+      userAgent: '',
+      userArn: '',
+    },
+    path: '/src',
+    protocol: 'HTTP/1.1',
+    requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
+    requestTimeEpoch: 1428582896000,
+    resourceId: '123456',
+    resourcePath: '/src',
+    stage: 'dev',
+  },
+  resource: '',
+  stageVariables: {},
+});
 
-        expect(result.statusCode).toEqual(200);
-        expect(result.body).toEqual(
-            JSON.stringify({
-                message: 'hello world',
-            }),
-        );
-    });
+describe('Unit test for app handler', function () {
+  it('verifies successful response', async () => {
+    const testUrl = 'https://example.com';
+
+    const event: APIGatewayProxyEvent = createEvent({ url: testUrl });
+    const result: APIGatewayProxyResult = await lambdaHandler(event);
+
+    expect(result.statusCode).toEqual(200);
+    expect(result.body).toEqual(
+      JSON.stringify({
+        message: 'Received URL',
+        url: testUrl,
+      }),
+    );
+  });
+
+  it('verifies missing URL parameter', async () => {
+    const event = createEvent({}); // No URL parameter provided
+
+    const result: APIGatewayProxyResult = await lambdaHandler(event);
+
+    expect(result.statusCode).toEqual(400);
+    expect(result.body).toEqual(
+      JSON.stringify({
+        message: 'URL query parameter is missing.',
+      }),
+    );
+  });
 });
